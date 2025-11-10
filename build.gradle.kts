6ab819e162f97627cb71af81a66495991b3270ef
Ruddy4761 <rudrathacker2005@gmail.com>
2025-11-10 13:28:05 +0530

    hi

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application") version "8.12.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 }
 
